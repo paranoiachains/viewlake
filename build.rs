6ac7f7b1bb02f44edf7f5785dfbaba7ee245fda0
paranoiachains <bluntsside@gmail.com>
2025-07-31 17:44:04 +0300

    +build.rs fix and build-win libs

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=/home/keira/rust/mbedtls/build-win/library");
+    println!("cargo:rustc-link-search=native=/home/keira/rust/viewlake/build-win/library");
 
     println!("cargo:rustc-link-lib=static=mbedtls");
     println!("cargo:rustc-link-lib=static=mbedx509");
     println!("cargo:rustc-link-lib=static=tfpsacrypto");
-
-    println!("cargo:rustc-link-lib=dylib=crypto"); // example
-    println!("cargo:rustc-link-lib=dylib=pthread"); // example
 }
