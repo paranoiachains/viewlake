30e901a3c8fcd6af23fc4fd31745398ca53d19b8
paranoiachains <bluntsside@gmail.com>
2025-07-31 12:14:21 +0300

    add lib.rs

---
+++ target/debug/deps/viewlake-66262d25c435da1d.d
@@ -0,0 +1,5 @@
+/home/keira/rust/viewlake/target/debug/deps/viewlake-66262d25c435da1d.d: src/main.rs
+
+/home/keira/rust/viewlake/target/debug/deps/libviewlake-66262d25c435da1d.rmeta: src/main.rs
+
+src/main.rs:
