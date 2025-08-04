4cd5cdde89c10998c3e171c8312be2c5e820fe71
paranoiachains <bluntsside@gmail.com>
2025-08-04 13:36:13 +0100

    fix fix fix

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@
 
 #[allow(non_snake_case)]
 #[unsafe(no_mangle)]
-#[no_mangle]
 pub fn main(_argc: i32, _argv: *const *const u8) -> u32 {
-    let mut stdout = stdout();
-    stdout.write_all(b"Hello, world!\n").unwrap();
-
     0
 }
