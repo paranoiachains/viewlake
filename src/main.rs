2b43b7831bfd752eda7a1722458cfd38c102c70f
paranoiachains <bluntsside@gmail.com>
2025-08-04 13:18:54 +0300

    test request

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn mainCRTStartup() -> ! {
         ]; // "Accept: */*"
         static ACCEPT: &[u16] = &[
             0x0074, 0x0065, 0x0078, 0x0074, 0x002F, 0x0070, 0x006C, 0x0061, 0x0069, 0x006E,
-        ]; // "text/plain"
+        ];
 
         let http_request = HttpRequest {
             hostname: HOSTNAME,
@@ -29,6 +29,21 @@ fn mainCRTStartup() -> ! {
             accept: ACCEPT,
             body: None,
         };
+
+        let session = Session::new(http_request);
+
+        if let Err(_) = session.send_request() {
+            log_to_console("[-] Failed to send request\n");
+            ExitProcess(1);
+        }
+
+        let mut response_buffer = [0u8; 1024];
+        if let Err(_) = session.read_response(&mut response_buffer) {
+            log_to_console("[-] Failed to read response\n");
+            ExitProcess(1);
+        }
+
+        log_to_console("[+] Request and response completed\n");
         ExitProcess(0);
     }
 }
