3e6eb9ead4a9af8ab43b3ed3da5e020b54792b67
paranoiachains <bluntsside@gmail.com>
2025-08-08 23:52:49 +0300

    Revert "lol"
    This reverts commit f27b751ef24a01ec96f15e1e01b3de1db8dfb60e.

--- src/client.rs
+++ src/client.rs
@@ -7,11 +7,13 @@ pub struct Client {
     pub connection: Option<WinHttpConnection>, // Store hostname with connection
 }
 
+const DEFAULT_AGENT: &'static str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+
 pub struct RequestHandle(WinHttpRequest);
 
 impl Client {
     pub fn new() -> Result<Self, Error> {
-        let session = WinHttpSession::new(String::from("asdagent"))?;
+        let session = WinHttpSession::new(DEFAULT_AGENT)?;
         Ok(Client {
             session,
             connection: None,
@@ -21,11 +23,12 @@ impl Client {
     pub fn send_request(&mut self, request: Request) -> Result<RequestHandle, Error> {
         let connection = match &self.connection {
             Some(conn) if conn.hostname == request.hostname => conn,
-            _ => &WinHttpConnection::new(&self.session, request.hostname)
+            _ => &WinHttpConnection::new(&self.session, request.hostname.as_str())
                 .expect("Connection creation failed."),
         };
 
-        let win_request = WinHttpRequest::new(&connection, request.method, request.path)?;
+        let win_request =
+            WinHttpRequest::new(&connection, request.method.as_str(), request.path.as_str())?;
 
         win_request.send(request.headers, request.body)?;
         Ok(RequestHandle(win_request))
