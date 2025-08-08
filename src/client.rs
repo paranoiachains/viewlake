0e9ec66cece39bfaca5da6baa6f3111032536503
paranoiachains <bluntsside@gmail.com>
2025-08-08 23:26:01 +0300

    LOL;;asdsad

--- src/client.rs
+++ src/client.rs
@@ -62,7 +62,7 @@ pub struct Response {
 
 impl Response {
     pub fn read_response_into_stdout(&self) {
-        std::io::stdout().write_all(self.data.as_bytes()).unwrap();
+        std::io::stdout().write(self.data.as_bytes()).unwrap();
     }
 }
 
