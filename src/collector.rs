f1390c572e0753494d9ae2332650de84a5beca60
paranoiachains <bluntsside@gmail.com>
2025-08-12 16:36:53 +0300

    asdasd

--- src/collector.rs
+++ src/collector.rs
@@ -1,8 +1,17 @@
+use std::ptr::null_mut;
+
 use windows::Wdk::System::SystemServices::*;
+use windows::Win32::Foundation::NO_ERROR;
 use windows::Win32::Foundation::STATUS_SUCCESS;
+use windows::Win32::NetworkManagement::IpHelper::GAA_FLAG_INCLUDE_PREFIX;
+use windows::Win32::NetworkManagement::IpHelper::GetAdaptersAddresses;
+use windows::Win32::NetworkManagement::IpHelper::IP_ADAPTER_ADDRESSES_LH;
+use windows::Win32::NetworkManagement::NetManagement::*;
+use windows::Win32::Networking::WinSock::AF_UNSPEC;
 use windows::Win32::System::SystemInformation::*;
 use windows::Win32::System::SystemServices::*;
 use windows::core::Error;
+use windows::core::PCWSTR;
 use windows::core::PWSTR;
 
 // This logic implies only logging/collecting, not pattern matching
@@ -12,13 +21,11 @@ pub struct SystemInfo {
     pub arch: String,
     pub product_type: String,
     pub version: String,
-    pub fqdn: String,
 }
 
 impl SystemInfo {
     pub fn collect() -> Result<Self, Error> {
         let (product_type, version) = Self::os()?;
-        let fqdn = Self::fqdn()?;
 
         let arch = match Architecture::get() {
             Architecture::AMD64 => "AMD64".to_string(),
@@ -30,7 +37,6 @@ impl SystemInfo {
             arch,
             product_type,
             version,
-            fqdn,
         })
     }
 
@@ -58,7 +64,36 @@ impl SystemInfo {
             }
         }
     }
+}
 
+#[derive(Debug)]
+enum Architecture {
+    AMD64,
+    ARM64,
+    Unknown,
+}
+
+impl Architecture {
+    fn get() -> Self {
+        unsafe {
+            let mut sysinfo: SYSTEM_INFO = std::mem::zeroed();
+            GetNativeSystemInfo(&mut sysinfo as *mut _);
+
+            let arch = sysinfo.Anonymous.Anonymous.wProcessorArchitecture;
+            match arch.0 {
+                9 => Architecture::AMD64,
+                12 => Architecture::ARM64,
+                _ => Architecture::Unknown,
+            }
+        }
+    }
+}
+
+pub struct NetworkInfo {
+    fqdn: String,
+}
+
+impl NetworkInfo {
     fn fqdn() -> Result<String, Error> {
         unsafe {
             let mut size: u32 = 0;
@@ -81,27 +116,90 @@ impl SystemInfo {
             Ok(String::from_utf16_lossy(&buffer))
         }
     }
-}
 
-#[derive(Debug)]
-enum Architecture {
-    AMD64,
-    ARM64,
-    Unknown,
-}
-
-impl Architecture {
-    fn get() -> Self {
+    fn domain_or_workgroup() -> Result<(String, String), Error> {
         unsafe {
-            let mut sysinfo: SYSTEM_INFO = std::mem::zeroed();
-            GetNativeSystemInfo(&mut sysinfo as *mut _);
-
-            let arch = sysinfo.Anonymous.Anonymous.wProcessorArchitecture;
-            match arch.0 {
-                9 => Architecture::AMD64,
-                12 => Architecture::ARM64,
-                _ => Architecture::Unknown,
+            let mut buffer = PWSTR::null();
+            let mut status_result = NETSETUP_JOIN_STATUS::default();
+            if NetGetJoinInformation(
+                PCWSTR::null(),
+                &mut buffer as *mut _,
+                &mut status_result as *mut _,
+            ) != NERR_Success
+            {
+                return Err(Error::from_win32());
             }
+
+            let domain_or_workgroup = if !buffer.is_null() {
+                let mut len = 0;
+                while *buffer.0.add(len) != 0 {
+                    len += 1;
+                }
+                let slice = std::slice::from_raw_parts(buffer.0, len);
+                String::from_utf16_lossy(slice)
+            } else {
+                String::new()
+            };
+
+            NetApiBufferFree(Some(buffer.0 as _));
+
+            let status = match status_result {
+                NetSetupUnjoined => "Unjoined".to_string(),
+                NetSetupWorkgroupName => "Workgroup".to_string(),
+                NetSetupDomainName => "Domain".to_string(),
+                _ => "Unknown".to_string(),
+            };
+
+            Ok((domain_or_workgroup, status))
+        }
+    }
+
+    fn network() -> Result<Vec<String>, Error> {
+        unsafe {
+            let mut size = 0u32;
+
+            GetAdaptersAddresses(
+                AF_UNSPEC.0 as u32,
+                GAA_FLAG_INCLUDE_PREFIX,
+                None,
+                None,
+                &mut size as *mut _,
+            );
+
+            let mut buffer = vec![0u8; size as usize];
+            let adapter_addresess = buffer.as_mut_ptr() as *mut IP_ADAPTER_ADDRESSES_LH;
+
+            let ret = GetAdaptersAddresses(
+                AF_UNSPEC.0 as u32,
+                GAA_FLAG_INCLUDE_PREFIX,
+                None,
+                Some(adapter_addresess),
+                &mut size as *mut _,
+            );
+
+            if ret != 0 {
+                return Err(Error::from_win32());
+            }
+
+            let mut current = adapter_addresess;
+            let mut adapter_vec: Vec<String> = Vec::new();
+            while !current.is_null() {
+                let adapter = &*current;
+                let name = if !adapter.FriendlyName.is_null() {
+                    adapter
+                        .FriendlyName
+                        .to_string()
+                        .expect("Error while converting friendly name to string")
+                } else {
+                    String::new()
+                };
+
+                adapter_vec.push(name);
+
+                current = adapter.Next;
+            }
+
+            Ok(adapter_vec)
         }
     }
 }
@@ -137,7 +235,5 @@ mod tests {
             "Unexpected product type: {}",
             os_info.product_type
         );
-
-        assert!(!os_info.fqdn.is_empty(), "FQDN is empty");
     }
 }
