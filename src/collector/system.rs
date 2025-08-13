ffe595078c4041239df28551bfbd105633082f19
paranoiachains <bluntsside@gmail.com>
2025-08-13 16:26:24 +0300

    divide by modules

---
+++ src/collector/system.rs
@@ -0,0 +1,78 @@
+use windows::Wdk::System::SystemServices::*;
+use windows::Win32::Foundation::STATUS_SUCCESS;
+use windows::Win32::System::SystemInformation::*;
+use windows::Win32::System::SystemServices::*;
+use windows::core::Error;
+
+#[derive(Debug)]
+pub struct SystemInfo {
+    pub arch: String,
+    pub product_type: String,
+    pub version: String,
+}
+
+impl SystemInfo {
+    pub fn collect() -> Result<Self, Error> {
+        let (product_type, version) = Self::os()?;
+
+        let arch = match Architecture::get() {
+            Architecture::AMD64 => "AMD64".to_string(),
+            Architecture::ARM64 => "ARM64".to_string(),
+            Architecture::Unknown => "Unknown".to_string(),
+        };
+
+        Ok(SystemInfo {
+            arch,
+            product_type,
+            version,
+        })
+    }
+
+    fn os() -> Result<(String, String), Error> {
+        unsafe {
+            let mut os_version: OSVERSIONINFOEXW = std::mem::zeroed();
+            let ntstatus = RtlGetVersion(&mut os_version as *mut _ as *mut OSVERSIONINFOW);
+
+            if ntstatus == STATUS_SUCCESS {
+                let product_type = match os_version.wProductType as u32 {
+                    VER_NT_WORKSTATION => "Workstation".to_string(),
+                    VER_NT_SERVER => "Server".to_string(),
+                    VER_NT_DOMAIN_CONTROLLER => "Domain Controller".to_string(),
+                    _ => "Unknown".to_string(),
+                };
+
+                let version = format!(
+                    "{}.{}.{}",
+                    os_version.dwMajorVersion, os_version.dwMinorVersion, os_version.dwBuildNumber
+                );
+
+                Ok((product_type, version))
+            } else {
+                Err(Error::from_win32())
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Architecture {
+    AMD64,
+    ARM64,
+    Unknown,
+}
+
+impl Architecture {
+    fn get() -> Self {
+        unsafe {
+            let mut sysinfo: SYSTEM_INFO = std::mem::zeroed();
+            GetNativeSystemInfo(&mut sysinfo as *mut _);
+
+            let arch = sysinfo.Anonymous.Anonymous.wProcessorArchitecture;
+            match arch.0 {
+                9 => Architecture::AMD64,
+                12 => Architecture::ARM64,
+                _ => Architecture::Unknown,
+            }
+        }
+    }
+}
