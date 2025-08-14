a880be20cd8997144ad7a879f76db614f29427b7
paranoiachains <bluntsside@gmail.com>
2025-08-14 10:28:44 +0300

    privileges

--- src/collector/user.rs
+++ src/collector/user.rs
@@ -1,22 +1,21 @@
 use windows::Win32::Foundation::*;
-use windows::Win32::Security::{
-    GetTokenInformation, LookupAccountSidW, SID_AND_ATTRIBUTES, SID_NAME_USE, TOKEN_GROUPS,
-    TOKEN_QUERY, TokenGroups,
-};
+use windows::Win32::Security::*;
 use windows::Win32::System::Threading::{GetCurrentProcess, OpenProcessToken};
 use windows::{Win32::System::WindowsProgramming::GetUserNameW, core::PWSTR};
 
 #[derive(Debug)]
 pub struct UserInfo {
     pub username: Option<String>,
-    pub groups: Vec<String>,
+    pub groups: Option<Vec<String>>,
+    pub privileges: Option<Vec<String>>,
 }
 
 impl UserInfo {
     pub fn collect() -> Self {
         UserInfo {
             username: Self::get_username().ok(),
-            groups: Self::get_user_groups().expect("Couldn't retrieve groups"),
+            groups: Self::get_user_groups().ok(),
+            privileges: Self::get_user_privileges().ok(),
         }
     }
 
@@ -77,10 +76,71 @@ impl UserInfo {
 
                 let group = String::from_utf16_lossy(&name[..cch_name as usize]);
                 let domain = String::from_utf16_lossy(&domain[..cch_domain as usize]);
-                groups.push(format!("{}\\{}", group, domain));
+
+                if domain.is_empty() {
+                    groups.push(group)
+                } else {
+                    groups.push(format!("{}\\{}", domain, group));
+                }
             }
 
             Ok(groups)
+        }
+    }
+
+    fn get_user_privileges() -> Result<Vec<String>, windows::core::Error> {
+        unsafe {
+            let mut token_handle: HANDLE = HANDLE::default();
+            let handle = GetCurrentProcess();
+
+            OpenProcessToken(handle, TOKEN_QUERY, &mut token_handle)?;
+
+            let mut return_length: u32 = 1024;
+            let mut buffer: Vec<u8> = vec![0; return_length as usize];
+
+            GetTokenInformation(
+                token_handle,
+                TokenPrivileges,
+                Some(buffer.as_mut_ptr() as *mut _),
+                return_length,
+                &mut return_length,
+            )?;
+
+            let token_privileges: *const TOKEN_PRIVILEGES =
+                buffer.as_ptr() as *const TOKEN_PRIVILEGES;
+            let privilege_count = (*token_privileges).PrivilegeCount as usize;
+
+            let privileges_ptr =
+                &(*token_privileges).Privileges as *const _ as *const LUID_AND_ATTRIBUTES;
+            let mut privileges = Vec::new();
+
+            for i in 0..privilege_count {
+                let laa = unsafe { *privileges_ptr.add(i) };
+
+                let mut name_buf = [0u16; 256];
+                let mut name_len = name_buf.len() as u32;
+
+                unsafe {
+                    LookupPrivilegeNameW(
+                        None,
+                        &laa.Luid,
+                        PWSTR(name_buf.as_mut_ptr()),
+                        &mut name_len,
+                    )?;
+                }
+
+                let name = String::from_utf16_lossy(&name_buf[..name_len as usize]);
+
+                let enabled = laa.Attributes & SE_PRIVILEGE_ENABLED.0 != 0;
+
+                privileges.push(format!(
+                    "{}{}",
+                    name,
+                    if enabled { " (Enabled)" } else { "" }
+                ));
+            }
+
+            Ok(privileges)
         }
     }
 }
@@ -91,8 +151,9 @@ mod tests {
 
     #[test]
     fn get_user_info() {
-        let uinfo = UserInfo::collect();
-        println!("username: {:?}", uinfo.username);
-        println!("token: {:?}", uinfo.groups);
+        let info = UserInfo::collect();
+        println!("{:?}", info);
+
+        assert!(info.username.is_some(), "Username should not be None");
     }
 }
