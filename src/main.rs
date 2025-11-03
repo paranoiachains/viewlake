c80072ea4bbae0bc37edffa43238b58489f2699f
paranoiachains <bluntsside@gmail.com>
2025-11-03 13:01:31 +0300

    display response

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use viewlake::send_initial;
 #[unsafe(no_mangle)]
 unsafe extern "C" fn main(_argc: i32, _argv: *const *const u8) -> u32 {
     collect().unwrap();
-    send_initial().unwrap();
+    let response = send_initial().unwrap();
+    println!("{}, {:?}", response.code, response.body);
 
     0
 }
