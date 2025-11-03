fe752e8667a8a5af8d92f9e147665171e733db4c
paranoiachains <bluntsside@gmail.com>
2025-11-03 11:46:45 +0300

    basic usage example in main

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
 pub mod collector;
 pub mod comms;
+
+use collector::SystemFingerprint;
+use windows::core::Result;
+
+pub fn collect() -> Result<SystemFingerprint> {
+    SystemFingerprint::collect()
+}
