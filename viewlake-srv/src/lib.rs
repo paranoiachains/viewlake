5e1b8d22668d45027101a2446509e83fdef5fe40
paranoiachains <bluntsside@gmail.com>
2025-11-07 23:37:44 +0300

    simple handler

--- viewlake-srv/src/lib.rs
+++ viewlake-srv/src/lib.rs
@@ -1,11 +1,22 @@
-use axum::routing::get;
+use axum::{Router, body::Bytes, http::StatusCode, routing::post};
 
 pub async fn run() -> Result<(), std::io::Error> {
-    let app = axum::Router::new().route("/", get(|| async {}));
+    let app = Router::new().route("/hi", post(hi));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3333")
         .await
-        .expect("Error while creating listener");
+        .expect("Failed to bind");
 
-    axum::serve(listener, app).await
+    println!("Listening on 127.0.0.1:3333");
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn hi(body: Bytes) -> StatusCode {
+    let body_str = String::from_utf8_lossy(&body);
+    println!("Received body: {}", body_str);
+
+    StatusCode::OK
 }
