8a7b3ef0d4a50c4d1907ad7a375f9686941a3684
paranoiachains <bluntsside@gmail.com>
2026-01-28 12:42:27 +0300

    revertA

--- viewlake-agent/src/beacon.rs
+++ viewlake-agent/src/beacon.rs
@@ -55,9 +55,6 @@ fn build_winhttp_headers(headers: &[HSTRING]) -> Vec<u16> {
 
     for header in headers {
         buf.extend(header.as_wide());
-        if headers.len() > 1 {
-            buf.extend("\r\n".encode_utf16());
-        }
     }
 
     buf.push(0);
