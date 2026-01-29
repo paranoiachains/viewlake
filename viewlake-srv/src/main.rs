328b88993d212dc7d7cc0934421a9d4eaa9a6ac5
paranoiachains <bluntsside@gmail.com>
2026-01-29 11:31:29 +0300

    add tracing

--- viewlake-srv/src/main.rs
+++ viewlake-srv/src/main.rs
@@ -1,5 +1,9 @@
+use tracing_subscriber::{EnvFilter, fmt};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    init_tracing();
+
     let addr = "127.0.0.1:3000";
     let cert_path = "ssl/cert.pem";
     let key_path = "ssl/key.pem";
@@ -7,4 +11,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     viewlake_srv::run(addr, cert_path, key_path).await?;
 
     Ok(())
+}
+
+fn init_tracing() {
+    fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .init();
 }
